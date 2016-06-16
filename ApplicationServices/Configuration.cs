e0767d409a605cdd6d8f3c56a1f600b8a71cea26
rfaes <mail@rfaes.com>
2016-06-16 22:44:48 +0200

    trying to start executable in the same directory as WC3Proxy

--- ApplicationServices/Configuration.cs
+++ ApplicationServices/Configuration.cs
@@ -1,14 +1,23 @@
 ﻿using System.Configuration;
+using System.Linq;
 
 namespace Foole.WC3Proxy.ApplicationServices
 {
     public class Configuration
     {
+        // App settings keys
+        private const string WC3ExecutablenameAppSettingsKey = "WC3ExecutableName";
+        private const string WC3ExpansionExecutablenameAppSettingsKey = "WC3ExpansionExecutableName";
+
+        // default values if app.config doesn't exist
+        private const string DefaultWC3ExecutableName = "War3.exe";
+        private const string DefaultWC3ExpansionExecutableName = "Frozen throne.exe";
+
         public string WC3ExecutableName
         {
             get
             {
-                return ConfigurationManager.AppSettings["WC3ExecutableName"];
+                return ConfigurationManager.AppSettings.AllKeys.Contains(WC3ExecutablenameAppSettingsKey) ? ConfigurationManager.AppSettings[WC3ExecutablenameAppSettingsKey] : DefaultWC3ExecutableName;
             }
         }
 
@@ -16,7 +25,7 @@ namespace Foole.WC3Proxy.ApplicationServices
         {
             get
             {
-                return ConfigurationManager.AppSettings["WC3ExpansionExecutableName"];
+                return ConfigurationManager.AppSettings.AllKeys.Contains(WC3ExpansionExecutablenameAppSettingsKey) ? ConfigurationManager.AppSettings[WC3ExpansionExecutablenameAppSettingsKey] : DefaultWC3ExpansionExecutableName;
             }
         }
     }
--- ApplicationServices/GameService.cs
+++ ApplicationServices/GameService.cs
@@ -10,12 +10,34 @@ namespace Foole.WC3Proxy.ApplicationServices
     {
         private const string Wc3RegistryPath= @"HKEY_CURRENT_USER\Software\Blizzard Entertainment\Warcraft III";
 
+        private readonly Configuration configuration;
+
+        public GameService(Configuration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public bool TryToStartGame(bool isExpansion)
         {
             try
             {
                 string programkey = isExpansion ? "ProgramX" : "Program";
                 var program = (string)Registry.GetValue(Wc3RegistryPath, programkey, null);
+
+                if (File.Exists(program))
+                {
+                    Process.Start(program);
+                    return true;
+                }
+
+                if (isExpansion)
+                {
+                    program = configuration.WC3ExpansionExecutableName;
+                }
+                else
+                {
+                    program = configuration.WC3ExecutableName;
+                }
 
                 if (File.Exists(program))
                 {
