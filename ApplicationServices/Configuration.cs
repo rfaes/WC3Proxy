2a5b594c92ae8ea9597653b2d827e3a8c878ba69
rfaes <mail@rfaes.com>
2016-06-16 22:34:46 +0200

    Added configuration to read out executable name from app.config it

---
+++ ApplicationServices/Configuration.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+
+namespace Foole.WC3Proxy.ApplicationServices
+{
+    public class Configuration
+    {
+        public string WC3ExecutableName
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["WC3ExecutableName"];
+            }
+        }
+
+        public string WC3ExpansionExecutableName
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["WC3ExpansionExecutableName"];
+            }
+        }
+    }
+}
